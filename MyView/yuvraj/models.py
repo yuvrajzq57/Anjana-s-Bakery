4b1dfadc5ae576e0ca0c454dbb6ff0fa41bfc0a0
Yuvraj <yuvrajzq57@gmail.com>
2022-03-01 01:01:17 +0530

    django_project_01

---
+++ MyView/yuvraj/models.py
@@ -0,0 +1,15 @@
+from email.headerregistry import Address
+from django.db import models
+
+# Create your models here.
+class About(models.Model):
+    Name = models.CharField(default='', max_length=123)
+    Address = models.CharField(default='', max_length=123)
+    Landmark = models.CharField(default='', max_length=123)
+    City = models.CharField(default='', max_length=123)
+    State = models.CharField(default='', max_length=123)
+    Zip = models.CharField(default='', max_length=123)
+    header_image = models.ImageField(null = True ,  blank=True)
+
+    def __str__(self):
+        return f"{self.Name} - {self.City}"
---
+++ MyView/yuvraj/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
+from yuvraj.models import About
+
+# Create your views here.
+def yuvraj(request):
+    return render(request, 'home.html')
+
+def about(request):
+   
+    # We can handle the form here .
+    if request.method == 'POST':
+        Name = request.POST.get('Name')
+        Address = request.POST.get('Address')
+        Landmark = request.POST.get('Landmark')
+        City = request.POST.get('City')
+        State = request.POST.get('State')
+        Zip = request.POST.get('Zip')
+        a = About(Name=(Name) , Address=(Address) , Landmark= (Landmark) ,City = (City) , State = (State) , Zip = (Zip) )
+        a.save()
+    else:
+        pass
+    return render(request, 'about.html')
+     
+
+def orders(request):
+    return render(request, 'orders.html')
+
+def cakes(request):
+    return render(request, 'cakes.html')
+
+
+ 
